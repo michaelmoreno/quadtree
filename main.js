cbb0ab38451d5423b2b04c4b9ed1e565fe83a095
michaelmoreno <michaelmoreno.dev@gmail.com>
2021-04-25 18:44:58 -0500

    Fully functional, ready to integrate.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const sizer = () => {
 window.addEventListener('resize', sizer())
 document.body.style.cssText = 'margin: 0; padding: 0; overflow: hidden; background-color: black'
 
-let boundary = new Rectangle(300, 300, canvas.width - 600, canvas.height - 600, 'top', 'green');
+let boundary = new Rectangle(10, 10, canvas.width - 20, canvas.height - 20, 'top', 'green');
 let qt = new Quadtree(boundary, 1);
 boundary.draw(ctx);
 function init() {
@@ -27,10 +27,10 @@ let mouseX;
 let mouseY;
 window.addEventListener('mousedown', () => mouseDown = true);
 window.addEventListener('mousemove', function(event) {
-  mouseX = event.x;
-  mouseY = event.y;
-  mouseDownAndMove = true;
   if (mouseDown) {
+    mouseX = event.x;
+    mouseY = event.y;
+    mouseDownAndMove = true;
   }
 });
 window.addEventListener('mouseup', function() {
@@ -39,25 +39,45 @@ window.addEventListener('mouseup', function() {
 })
 
 
-const randomX = Math.random() * ((qt.boundary.x + qt.boundary.w) - qt.boundary.x) + qt.boundary.x;
-const randomY = Math.random() * ((qt.boundary.y + qt.boundary.h) - qt.boundary.y) + qt.boundary.y;
+
+let randomX = Math.random() * ((qt.boundary.x + qt.boundary.w) - qt.boundary.x) + qt.boundary.x;
+let randomY = Math.random() * ((qt.boundary.y + qt.boundary.h) - qt.boundary.y) + qt.boundary.y;
 let range = new Rectangle(randomX, randomY, 400, 375);
 function render() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   qt.render();
   if (mouseDownAndMove) {
+    range.x = mouseX;
+    range.y = mouseY;
+    ctx.beginPath();
+    ctx.closePath();
+    ctx.fillStyle = 'blue',
+    ctx.fill();
+    
   }
   ctx.beginPath();
-  ctx.rect(randomX, randomY, 400, 375);
+  range.draw();
+  // ctx.rect(randomX, randomY, 400, 375);
   ctx.strokeStyle = 'red';
   ctx.lineWidth = 5;
   ctx.stroke();
   ctx.closePath();
   ctx.lineWidth = 1
-  console.log(qt.query(range));
+  qt.query(range);
+  ctx.fillStyle = 'white';
+  ctx.font = '25px arial';
+  ctx.fillText(qt.query(range)[0].length, range.x-10, range.y-10);
+  ctx.closePath();
   
-  
-  // requestAnimationFrame(render);
+  requestAnimationFrame(render);
+}
+
+const grabBread = () => {
+  //
+}
+
+const makeSandwich = () => {
+  grabBread();
 }
 
 init();
