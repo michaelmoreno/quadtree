de4d1eb642fdc60d4217c15f3d90acb36ec67c42
michaelmoreno <michaelmoreno.dev@gmail.com>
2021-04-25 09:13:49 -0500

    Contains point working.

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ window.addEventListener('resize', sizer())
 document.body.style.cssText = 'margin: 0; padding: 0; overflow: hidden;'
 
 function init() {
-  let boundary = new Rectangle(0, 0, canvas.width, canvas.height, 'top', 'green');
-  let qt = new Quadtree(boundary, 2);
+  let boundary = new Rectangle(10, 10, canvas.width - 20, canvas.height - 20, 'top', 'green');
+  let qt = new Quadtree(boundary, 1);
   console.log(qt);
   boundary.draw(ctx);
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 30; i++) {
     let p = new Point(Math.random() * canvas.width, Math.random() * canvas.height);
     p.draw();
     qt.insert(p);
--- quadtree.js
+++ quadtree.js
@@ -10,6 +10,8 @@ class Point {
     ctx.arc(this.x, this.y, 5, 0, Math.PI * 2, false);
     ctx.fillStyle = 'blue';
     ctx.fill();
+    ctx.font = '15px arial';
+    ctx.fillText(`${this.x}, ${this.y}`, this.x-30, this.y+30)
     ctx.closePath();
   }
 }
@@ -24,7 +26,6 @@ class Rectangle {
     this.color = color
   }
   contains(point) {
-    console.log('1');
     ctx.beginPath();
     function arcs(x,y,name,color){
       ctx.beginPath();
@@ -32,21 +33,31 @@ class Rectangle {
       ctx.closePath();
       ctx.fillStyle = color
       ctx.fill();
-      ctx.font = '25px arial';
-      ctx.fillText(name, x-30, y-30)
+      ctx.font = '15px arial';
+      ctx.fillText(`${x}, ${y}`, x-80, y-10)
     }
     arcs(this.x, this.y, this.name, this.color);
-    arcs(this.x, this.y + this.h, this.name, this.color)
     arcs(this.x + this.w, this.y, this.name, this.color)
     arcs(this.x+this.w, this.y+this.h, this.name, this.color)
-    // ctx.arc(this.x, this.y+this.h, 10, 0, Math.PI * 2, false);
-    // ctx.arc(this.x+this.w, this.y, 10, 0, Math.PI * 2, false);
+    ctx.arc(this.x, this.y+this.h, 10, 0, Math.PI * 2, false);
     // ctx.fillStyle = this.color;
     // ctx.fillText(this.name, this.x + this.w-50, this.y+40)
     ctx.closePath()
     ctx.fill();
+    console.log(' ');
+    console.log(this.name);
+    console.log(`point.x ${point.x}`);
+    console.log(`this.x: ${this.x}`);
+    console.log(`point.x > this.x: ${point.x > this.x}`);
+    console.log(`this.x + this.w: ${this.x+this.w}`);
+    console.log(`point.x < this.x+this.w: ${point.x < this.x+this.w}`);
+    console.log(`point.y: ${point.y}`);
+    console.log(`this.y: ${this.y}`);
+    console.log(`point.y > this.y: ${point.y > this.y}`);
+    console.log(`this.h: ${this.h}`);
+    console.log(`point.y < this.y + this.h: ${point.y < this.y + this.h}`);
     return (
-      (point.x > this.x && point.x < this.w) && 
+      (point.x > this.x && point.x < this.x+this.w) && 
       (point.y > this.y && point.y < this.y + this.h)
     )
     
@@ -57,10 +68,11 @@ class Rectangle {
   }
   draw() {
     ctx.beginPath();
+    ctx.arc(this.x,this.y, 10,0, Math.PI * 2, false);
     ctx.moveTo(this.x, this.y);
     ctx.lineTo(this.x + this.w, this.y)
-    ctx.lineTo(this.x + this.w, this.y - this.h);
-    ctx.lineTo(this.x, this.y - this.h);
+    ctx.lineTo(this.x + this.w, this.y + this.h);
+    ctx.lineTo(this.x, this.y + this.h);
     ctx.lineTo(this.x, this.y);
     ctx.strokeStyle = 'green';
     ctx.stroke();
@@ -81,29 +93,29 @@ class Quadtree {
     const { x, y, w, h } = this.boundary,
       halfWidth = w/2,
       halfHeight = h/2;
-    
-    let neBoundary = new Rectangle(x + halfWidth, y + halfHeight, halfWidth, -halfHeight, 'northeast', 'red');
-    this.northeast = new Quadtree(neBoundary, this.capacity);
+      
+    let neBoundary = new Rectangle(x + halfWidth, y, halfWidth, halfHeight, 'northeast');
+    this.northeast = new Quadtree(neBoundary, this.capacity)
     this.northeast.boundary.draw();
-
-    // let seBoundary = new Rectangle(x + halfWidth, y + halfHeight, halfWidth, -halfHeight);
-    // this.southeast = new Quadtree(seBoundary, this.capacity)
-    // this.southeast.boundary.draw();
     
-    // let swBoundary = new Rectangle(x + halfWidth, y + halfHeight, -halfWidth, -halfHeight)
-    // this.southwest = new Quadtree(swBoundary, this.capacity)
-    // this.southwest.boundary.draw();
+    let seBoundary = new Rectangle(x + halfWidth, y + halfHeight, halfWidth, halfHeight, 'southeast', 'red');
+    this.southeast = new Quadtree(seBoundary, this.capacity);
+    this.southeast.boundary.draw();
 
-    // let nwBoundary = new Rectangle(x + halfWidth, y + halfHeight, -halfWidth, halfHeight)
-    // this.northwest = new Quadtree(nwBoundary, this.capacity)
-    // this.northwest.boundary.draw();
+    
+    let swBoundary = new Rectangle(x, y + halfHeight, halfWidth, halfHeight, 'southwest')
+    this.southwest = new Quadtree(swBoundary, this.capacity)
+    this.southwest.boundary.draw();
+    
+    let nwBoundary = new Rectangle(x, y, halfWidth, halfHeight)
+    this.northwest = new Quadtree(nwBoundary, this.capacity)
+    this.northwest.boundary.draw();
 
     this.divided = true;
   }
   insert(point) {
     this.insertionAttempted = true;
     if (!this.boundary.contains(point)) {
-      console.log('fail');
       return;
     }
     this.containsPoints = true;
@@ -114,17 +126,13 @@ class Quadtree {
       if (!this.divided) {
         console.log('divide');
         this.subdivide();
-      }
-      console.log(this.northeast);
-      if (this.northeast) {
-        console.log('yes');
       } else {
-        console.log('no');
+        console.log('stop dividing');
       }
       this.northeast.insert(point)
-      // this.northwest.insert(point)
-      // this.southeast.insert(point)
-      // this.southwest.insert(point)
+      this.southeast.insert(point)
+      this.southwest.insert(point)
+      this.northwest.insert(point)
     }
   }
 }
