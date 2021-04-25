ecbdccc3190e7808e7df96809e2e05a74de56025
michaelmoreno <michaelmoreno.dev@gmail.com>
2021-04-25 11:23:55 -0500

    Render working.

--- main.js
+++ main.js
@@ -8,23 +8,47 @@ const sizer = () => {
 window.addEventListener('resize', sizer())
 document.body.style.cssText = 'margin: 0; padding: 0; overflow: hidden;'
 
+let boundary = new Rectangle(10, 10, canvas.width - 20, canvas.height - 20, 'top', 'green');
+let qt = new Quadtree(boundary, 1);
+boundary.draw(ctx);
 function init() {
-  let boundary = new Rectangle(10, 10, canvas.width - 20, canvas.height - 20, 'top', 'green');
-  let qt = new Quadtree(boundary, 1);
+  // for (let i = 0; i < 200; i++) {
+  //   let p = new Point(Math.random() * canvas.width, Math.random() * canvas.height);
+  //   p.draw();
+  //   qt.insert(p);
+  // }
   console.log(qt);
-  boundary.draw(ctx);
-  for (let i = 0; i < 30; i++) {
-    let p = new Point(Math.random() * canvas.width, Math.random() * canvas.height);
-    p.draw();
-    qt.insert(p);
-  }
 }
 
+let mouseDown = false;
+let mouseDownAndMove = false;
+let mouseX;
+let mouseY;
+window.addEventListener('mousedown', () => mouseDown = true);
+window.addEventListener('mousemove', function(event) {
+  if (mouseDown) {
+    mouseX = event.x;
+    mouseY = event.y;
+    mouseDownAndMove = true;
+  }
+});
+window.addEventListener('mouseup', () => mouseDown = false)
+
+let points = [];
 function render() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (mouseDownAndMove) {
+    console.log('pressed');
+    let p = new Point(mouseX, mouseY);
+    qt.insert(p);
+  }
+  qt.render();
+  
+  requestAnimationFrame(render);
 }
 
 init();
+render();
 
 export {
   ctx,
--- quadtree.js
+++ quadtree.js
@@ -10,65 +10,25 @@ class Point {
     ctx.arc(this.x, this.y, 5, 0, Math.PI * 2, false);
     ctx.fillStyle = 'blue';
     ctx.fill();
-    ctx.font = '15px arial';
-    ctx.fillText(`${this.x}, ${this.y}`, this.x-30, this.y+30)
     ctx.closePath();
   }
 }
 
 class Rectangle {
-  constructor(x,y,w,h, name,color) {
+  constructor(x,y,w,h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
-    this.name = name;
-    this.color = color
   }
   contains(point) {
-    ctx.beginPath();
-    function arcs(x,y,name,color){
-      ctx.beginPath();
-      ctx.arc(x,y, 10, 0, Math.PI * 2, false)
-      ctx.closePath();
-      ctx.fillStyle = color
-      ctx.fill();
-      ctx.font = '15px arial';
-      ctx.fillText(`${x}, ${y}`, x-80, y-10)
-    }
-    arcs(this.x, this.y, this.name, this.color);
-    arcs(this.x + this.w, this.y, this.name, this.color)
-    arcs(this.x+this.w, this.y+this.h, this.name, this.color)
-    ctx.arc(this.x, this.y+this.h, 10, 0, Math.PI * 2, false);
-    // ctx.fillStyle = this.color;
-    // ctx.fillText(this.name, this.x + this.w-50, this.y+40)
-    ctx.closePath()
-    ctx.fill();
-    console.log(' ');
-    console.log(this.name);
-    console.log(`point.x ${point.x}`);
-    console.log(`this.x: ${this.x}`);
-    console.log(`point.x > this.x: ${point.x > this.x}`);
-    console.log(`this.x + this.w: ${this.x+this.w}`);
-    console.log(`point.x < this.x+this.w: ${point.x < this.x+this.w}`);
-    console.log(`point.y: ${point.y}`);
-    console.log(`this.y: ${this.y}`);
-    console.log(`point.y > this.y: ${point.y > this.y}`);
-    console.log(`this.h: ${this.h}`);
-    console.log(`point.y < this.y + this.h: ${point.y < this.y + this.h}`);
     return (
       (point.x > this.x && point.x < this.x+this.w) && 
       (point.y > this.y && point.y < this.y + this.h)
     )
-    
-    // return (point.x < this.x + this.w &&
-    //   point.x < this.x + this.w &&
-    //   point.y > this.y - this.h &&
-    //   point.y < this.y + this.h)
   }
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x,this.y, 10,0, Math.PI * 2, false);
     ctx.moveTo(this.x, this.y);
     ctx.lineTo(this.x + this.w, this.y)
     ctx.lineTo(this.x + this.w, this.y + this.h);
@@ -86,53 +46,53 @@ class Quadtree {
     this.capacity = n;
     this.points = [];
     this.divided = false;
-    this.insertionAttempted = false;
-    this.containsPoints = false;
   }
   subdivide() {
     const { x, y, w, h } = this.boundary,
       halfWidth = w/2,
       halfHeight = h/2;
       
-    let neBoundary = new Rectangle(x + halfWidth, y, halfWidth, halfHeight, 'northeast');
+    let neBoundary = new Rectangle(x + halfWidth, y, halfWidth, halfHeight);
     this.northeast = new Quadtree(neBoundary, this.capacity)
-    this.northeast.boundary.draw();
     
-    let seBoundary = new Rectangle(x + halfWidth, y + halfHeight, halfWidth, halfHeight, 'southeast', 'red');
+    let seBoundary = new Rectangle(x + halfWidth, y + halfHeight, halfWidth, halfHeight);
     this.southeast = new Quadtree(seBoundary, this.capacity);
-    this.southeast.boundary.draw();
 
     
-    let swBoundary = new Rectangle(x, y + halfHeight, halfWidth, halfHeight, 'southwest')
+    let swBoundary = new Rectangle(x, y + halfHeight, halfWidth, halfHeight)
     this.southwest = new Quadtree(swBoundary, this.capacity)
-    this.southwest.boundary.draw();
     
     let nwBoundary = new Rectangle(x, y, halfWidth, halfHeight)
     this.northwest = new Quadtree(nwBoundary, this.capacity)
-    this.northwest.boundary.draw();
 
     this.divided = true;
   }
   insert(point) {
-    this.insertionAttempted = true;
     if (!this.boundary.contains(point)) {
       return;
     }
-    this.containsPoints = true;
-    
     if (this.points.length < this.capacity) {
       this.points.push(point)
     } else {
       if (!this.divided) {
-        console.log('divide');
         this.subdivide();
-      } else {
-        console.log('stop dividing');
       }
+
       this.northeast.insert(point)
       this.southeast.insert(point)
       this.southwest.insert(point)
       this.northwest.insert(point)
+    }
+  }
+  render() {
+    this.boundary.draw();
+    this.points.forEach(p => p.draw());
+    
+    if (this.divided) {
+      this.northeast.render();
+      this.southeast.render();
+      this.southwest.render();
+      this.northwest.render();
     }
   }
 }
