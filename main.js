10260814b46ec4da75a437cf56addcbc82c31e5f
michaelmoreno <michaelmoreno.dev@gmail.com>
2021-04-25 13:51:47 -0500

    Intersect check implemented.

--- main.js
+++ main.js
@@ -8,17 +8,16 @@ const sizer = () => {
 window.addEventListener('resize', sizer())
 document.body.style.cssText = 'margin: 0; padding: 0; overflow: hidden;'
 
-let boundary = new Rectangle(10, 10, canvas.width - 20, canvas.height - 20, 'top', 'green');
-let qt = new Quadtree(boundary, 1);
-boundary.draw(ctx);
-function init() {
-  // for (let i = 0; i < 200; i++) {
-  //   let p = new Point(Math.random() * canvas.width, Math.random() * canvas.height);
-  //   p.draw();
-  //   qt.insert(p);
-  // }
-  console.log(qt);
-}
+// let boundary = new Rectangle(10, 10, canvas.width - 20, canvas.height - 20, 'top', 'green');
+// let qt = new Quadtree(boundary, 1);
+// boundary.draw(ctx);
+// function init() {
+//   for (let i = 0; i < 300; i++) {
+//     let p = new Point(Math.random() * canvas.width, Math.random() * canvas.height);
+//     qt.insert(p);
+//   }
+//   console.log(qt);
+// }
 
 let mouseDown = false;
 let mouseDownAndMove = false;
@@ -34,21 +33,41 @@ window.addEventListener('mousemove', function(event) {
 });
 window.addEventListener('mouseup', () => mouseDown = false)
 
-let points = [];
+// console.log(qt);
+// let points = [];
 function render() {
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if (mouseDownAndMove) {
-    console.log('pressed');
-    let p = new Point(mouseX, mouseY);
-    qt.insert(p);
-  }
-  qt.render();
+  // if (mouseDownAndMove) {
+  //   console.log('pressed');
+  //   let p = new Point(mouseX, mouseY);
+  //   qt.insert(p);
+  // }
+  // qt.render();
+
+  // ctx.beginPath();
+  // let range = new Rectangle(250, 250, 207, 175);
+  // let points = qt.query(range);
+  // console.log(points);
+  
+  // ctx.rect(range.x, range.y, range.w * 2, range.h*2);
+  // ctx.strokeStyle = 'red';
+  // ctx.stroke();
   
   requestAnimationFrame(render);
 }
 
-init();
-render();
+
+let boundary = new Rectangle(canvas.width/2, canvas.height/2, 300, 200)
+boundary.draw();
+
+let range = new Rectangle(canvas.width/2+301, canvas.height/2+205, 300, 200);
+range.draw();
+
+console.log(boundary.intersects(range));
+
+
+// init();
+// render();
 
 export {
   ctx,
--- quadtree.js
+++ quadtree.js
@@ -23,10 +23,47 @@ class Rectangle {
   }
   contains(point) {
     return (
-      (point.x > this.x && point.x < this.x+this.w) && 
-      (point.y > this.y && point.y < this.y + this.h)
+      (point.x >= this.x && point.x <= this.x+this.w) && 
+      (point.y >= this.y && point.y <= this.y + this.h)
     )
   }
+
+  intersects(range) {
+    console.log(`range:`);
+    console.log(range);
+    console.log(`this`);
+    console.log(this);
+    
+    
+    ctx.beginPath();
+    ctx.arc(range.x, range.y, 10, 0, Math.PI * 2, false);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+
+    return !(
+      range.x+range.w < this.x ||
+      range.x > this.x+this.w ||
+      range.y+range.h < this.y ||
+      range.y > this.y+this.h
+    );
+    // if (
+    //   (range.x < this.x+this.w && range.x + range.w > this.x+this.w) ||
+    //   (range.x+range.w > this.x && range.x < this.x) ||
+    //   (range.y < this.y && range.y+range.h > this.y) ||
+    //   (range.y+range.h > this.y && range.y < this.y)); {
+    //     console.log('true');
+    //   }
+
+    // return (range.x-range.w > this.x+this.w ||
+    //   range.x+this.w < this.x-this.w ||
+    //   range.y-range.h > this.y+this.h ||
+    //   range.y+this.h < this.y-this.h)
+  }
+  
   draw() {
     ctx.beginPath();
     ctx.moveTo(this.x, this.y);
@@ -58,7 +95,6 @@ class Quadtree {
     let seBoundary = new Rectangle(x + halfWidth, y + halfHeight, halfWidth, halfHeight);
     this.southeast = new Quadtree(seBoundary, this.capacity);
 
-    
     let swBoundary = new Rectangle(x, y + halfHeight, halfWidth, halfHeight)
     this.southwest = new Quadtree(swBoundary, this.capacity)
     
@@ -69,25 +105,58 @@ class Quadtree {
   }
   insert(point) {
     if (!this.boundary.contains(point)) {
-      return;
+      return false;
     }
     if (this.points.length < this.capacity) {
       this.points.push(point)
+      return true
     } else {
       if (!this.divided) {
         this.subdivide();
       }
 
-      this.northeast.insert(point)
-      this.southeast.insert(point)
-      this.southwest.insert(point)
-      this.northwest.insert(point)
+      if (this.northeast.insert(point)) {
+        return true;
+      }
+      if (this.southeast.insert(point)) {
+        return true
+      }
+      if (this.southwest.insert(point)) {
+        return true
+      }
+      if (this.northwest.insert(point)) {
+        return true;
+      }
     }
   }
+
+  query(range) {
+    let found = ['1'];
+    
+    if (!this.boundary.intersects(range)) {
+      console.log('no');
+      return;
+    } else {
+      console.log('yes');
+      for (let p of this.points) {
+        if (range.contains(p)) {
+          found.push(p);
+        }
+      }
+      if (this.divided) {
+        found.concat(this.northeast.query(range));
+        found.concat(this.southeast.query(range));
+        found.concat(this.southwest.query(range));
+        found.concat(this.northwest.query(range));
+      }
+      return found
+    }
+  }
+  
   render() {
     this.boundary.draw();
     this.points.forEach(p => p.draw());
-    
+
     if (this.divided) {
       this.northeast.render();
       this.southeast.render();
